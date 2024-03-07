38958a20eab7cadcc85781c49eb9effc33e988c2
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-07 12:44:23 -0600

    Added route index aand show and method for index

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,6 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+        render :index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/employees" => "employees#index"
+  get "/employees/:id" => "employees#show"
 end

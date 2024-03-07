566a76eca6e29eb366d176927491f1cb021d4d28
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-07 14:26:21 -0600

    Added test for create

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -4,4 +4,13 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "create" do 
+    assert_difference"Employee.count", 1 do
+      post "/employees.json", params: {first_name: "test", last_name: "test", email: "test@hotmail.com", phone: "30958340", title: "test", department_id:1}
+      assert_response 200
+    end
+    assert_difference "Employee.count", 0 do
+      post "/employees.json", params: {first_name: "test", last_name: "test", email: "test@hotmail.com", phone: "30958340", title: "test", department_id:1}
+      assert_response 422
+    end
 end

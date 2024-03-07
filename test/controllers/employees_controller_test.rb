458fa348bdc6d2fb1222d1a525c213883d9c3e53
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-07 15:21:22 -0600

    Added test for index and show

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -5,12 +5,26 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   test "create" do 
-    assert_difference"Employee.count", 1 do
-      post "/employees.json", params: {first_name: "test", last_name: "test", email: "test@hotmail.com", phone: "30958340", title: "test", department_id:1}
+    assert_difference "Employee.count", 1 do
+      post "/employees.json", params: {first_name: "test", last_name: "test", email: "test@hotmail.com", phone: "30958340", title: "test", department_id: Department.first.id}
       assert_response 200
     end
     assert_difference "Employee.count", 0 do
-      post "/employees.json", params: {first_name: "test", last_name: "test", email: "test@hotmail.com", phone: "30958340", title: "test", department_id:1}
+      post "/employees.json", params: {first_name: "test", last_name: "test", email: "test@hotmail.com", phone: "30958340", title: "test", department_id: Department.first.id}
       assert_response 422
     end
+  end
+  test "index" do
+    get "/employees.json"
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal Employee.count, data.length
+  end
+  test "show" do
+    employee = Employee.first
+    get "/employees/#{Employee.first.id}.json"
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal employee.first_name,data["first_name"]
+  end
 end
